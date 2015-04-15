b3e7ea4c8b93ad23f9709e5b6448debf699869f2
a.m. hasler <amhasler@gmail.com>
2015-04-15 15:01:19 -0400

    get rid of logo

--- app/components/page-one-map.js
+++ app/components/page-one-map.js
@@ -7,6 +7,9 @@ export default Ember.Component.extend({
 			center: { lat: -34.397, lng: 150.644},
 			zoom: 8
 		};
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		setTimeout(function(){
+			var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		}, 1000);
+
 	}
 });
