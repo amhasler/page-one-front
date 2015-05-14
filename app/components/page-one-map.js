614da6de4ee869d0a773b8dbf4103b7bf02d61f2
a.m. hasler <amhasler@gmail.com>
2015-05-14 14:47:46 -0400

    git add . added all markers and zoom bounds fit markers

--- app/components/page-one-map.js
+++ app/components/page-one-map.js
@@ -3,13 +3,106 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	classNames: ['map'],
 	didInsertElement: function() {
-		var mapOptions = {
-			center: { lat: -34.397, lng: 150.644},
-			zoom: 8
-		};
+		var self = this;
+		//google.maps.event.addDomListener(window, 'load', this.get('initializeMap'));
 		setTimeout(function(){
-			var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-		}, 1000);
+			self.initializeMap();
+		},100)
+	},
+	initializeMap: function() {
+		var self = this;
+		var works = this.get('works').toArray()
+		
+		// var brooklyn = new google.maps.LatLng(40.6743890, -73.9455);
+		var MY_MAPTYPE_ID = 'custom_style'
 
+		var featureOpts = [
+		  {
+		    "featureType": "administrative",
+		    "stylers": [
+		      { "visibility": "off" }
+		    ]
+		  },{
+		    "featureType": "landscape.man_made",
+		    "stylers": [
+		      { "visibility": "off" }
+		    ]
+		  },{
+		    "featureType": "poi",
+		    "stylers": [
+		      { "visibility": "off" }
+		    ]
+		  },{
+		    "featureType": "road",
+		    "stylers": [
+		      { "visibility": "off" }
+		    ]
+		  },{
+		    "featureType": "transit",
+		    "stylers": [
+		      { "visibility": "off" }
+		    ]
+		  },{
+		    "featureType": "water",
+		    "stylers": [
+		      { "lightness": 36 }
+		    ]
+		  }
+		]
+	  
+	  var mapOptions = {
+	    zoom: 12,
+    	center: new google.maps.LatLng(0, 0),
+	    panControl: false,
+	    zoomControl: false,
+	    mapTypeControl: false,
+	    streetViewControl: false,
+	    mapTypeId: MY_MAPTYPE_ID
+	  };
+
+
+	  var map = new google.maps.Map(document.getElementById('map-canvas'),
+	      mapOptions);
+
+	  this.setMarkers(map,works)
+
+	  var styledMapOptions = {
+	    name: 'Custom Style'
+	  };
+
+	  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+	  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+	},
+	setMarkers: function(map,locations) {
+		var self = this;
+		//var lat = map.getCenter().lat(); 
+    //var lng = map.getCenter().lng(); 
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < locations.length; i++) {
+	    var work = locations[i]
+	    var geocoder = new google.maps.Geocoder();
+			geocoder.geocode({'address': work.get('place')}, function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					var myLatLng = new google.maps.LatLng(
+						results[0].geometry.location.lat(), 
+						results[0].geometry.location.lng()
+					);
+					var marker = new google.maps.Marker({
+			      position: myLatLng,
+			      map: map,
+				  });
+				  bounds.extend(myLatLng);
+				  map.fitBounds(bounds);
+				}
+			});
+  	}
+  	//console.log(bounds)
+  	return bounds  
 	}
 });
+
+
+
+
+
